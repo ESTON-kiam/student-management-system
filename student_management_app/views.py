ea5a8f5ccd3b799388976a8e1b9afbb9ef269bc0
ESTON-kiam <engestonbrandonkiama@gmail.com>
2024-06-24 16:00:16 +0300

    student management system

---
+++ student_management_app/views.py
@@ -0,0 +1,131 @@
+from django.shortcuts import render, HttpResponse, redirect, HttpResponseRedirect
+from django.contrib.auth import logout, authenticate, login
+from .models import CustomUser, Staffs, Students, AdminHOD
+from django.contrib import messages
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def loginUser(request):
+    return render(request, 'login_page.html')
+
+
+def doLogin(request):
+    if request.method == "POST":
+        email_id = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if not (email_id and password):
+            messages.error(request, "Please provide all the details!!")
+            return render(request, 'login_page.html')
+
+        user = authenticate(request, email=email_id, password=password)
+
+        if not user:
+            messages.error(request, 'Invalid Login Credentials!!')
+            return render(request, 'login_page.html')
+
+        login(request, user)
+
+        if user.user_type == CustomUser.STUDENT:
+            return redirect('student_home/')
+        elif user.user_type == CustomUser.STAFF:
+            return redirect('staff_home/')
+        elif user.user_type == CustomUser.HOD:
+            return redirect('admin_home/')
+
+    return render(request, 'login_page.html')
+
+
+def registration(request):
+    return render(request, 'registration.html')
+
+
+def get_user_type_from_email(email):
+    if 'student' in email:
+        return CustomUser.STUDENT
+    elif 'staff' in email:
+        return CustomUser.STAFF
+    elif 'hod' in email:
+        return CustomUser.HOD
+    return None
+
+
+def doRegistration(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email_id = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirmPassword')
+
+        if not (email_id and password and confirm_password):
+            messages.error(request, 'Please provide all the details!!')
+            return render(request, 'registration.html')
+
+        if password != confirm_password:
+            messages.error(request, 'Both passwords should match!!')
+            return render(request, 'registration.html')
+
+        if CustomUser.objects.filter(email=email_id).exists():
+            messages.error(request, 'User with this email id already exists. Please proceed to login!!')
+            return render(request, 'registration.html')
+
+        user_type = get_user_type_from_email(email_id)
+
+        if user_type is None:
+            messages.error(request,
+                           "Please use valid format for the email id: '<username>.<staff|student|hod>@<college_domain>'")
+            return render(request, 'registration.html')
+
+        username = email_id.split('@')[0].split('.')[0]
+
+        if CustomUser.objects.filter(username=username).exists():
+            messages.error(request, 'User with this username already exists. Please use different username')
+            return render(request, 'registration.html')
+
+        user = CustomUser()
+        user.username = username
+        user.email = email_id
+        user.set_password(password)  # Use set_password to hash the password
+        user.user_type = user_type
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+
+        if user_type == CustomUser.STAFF:
+            Staffs.objects.create(admin=user)
+        elif user_type == CustomUser.STUDENT:
+            Students.objects.create(admin=user)
+        elif user_type == CustomUser.HOD:
+            AdminHOD.objects.create(admin=user)
+
+        messages.success(request, 'Registration successful! Please login.')
+        return render(request, 'login_page.html')
+    return render(request, 'registration.html')
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def get_user_type_from_email(email_id):
+    """
+    Returns CustomUser.user_type corresponding to the given email address
+    email_id should be in the following format:
+    '<username>.<staff|student|hod>@<college_domain>'
+    eg.: 'abhishek.staff@jecrc.com'
+    """
+    try:
+        email_id = email_id.split('@')[0]
+        email_user_type = email_id.split('.')[1]
+        return CustomUser.EMAIL_TO_USER_TYPE_MAP[email_user_type]
+    except:
+        return None
